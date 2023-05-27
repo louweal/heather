e4b837bff8430f55ef09a1e30a1fa30502ee9508
louweal <anneloeslouwe@live.nl>
2023-05-27 16:02:14 +0200

    changes header

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,13 @@
 <template>
   <nav class="header fixed-top p-2" id="header" ref="header">
     <div class="d-flex justify-content-between align-items-center">
-      <nuxt-link to="/" event="" @click.native="scrollToTop()" class="header-logo me-4" aria-label="to homepage"> logo </nuxt-link>
-
+      <nuxt-link to="/" event="" @click.native="scrollToTop()" class="header-logo me-4" aria-label="to homepage">
+        <div class="logo"></div>
+      </nuxt-link>
       <nuxt-link to="/app">App</nuxt-link>
 
       <div class="ms-auto d-none d-lg-flex align-items-center gap-2 gap-md-4">
-        <div class="btn btn-primary">Button</div>
+        <div class="btn btn-primary">Sign in / Register</div>
       </div>
       <div class="d-lg-none" @click="$store.commit('pushmenu/toggle')">
         <i class="bi lh-1" style="font-size: 38px" :class="$store.state.pushmenu.open ? 'bi-x-lg' : 'bi-list'"></i>
@@ -63,5 +64,19 @@ export default {
   background-color: #fff;
   box-shadow: 0 0.125rem 0.25rem rgba(#000, 0.075);
   border-bottom: 1px solid rgba(#000, 0.175);
+
+  .logo {
+    background-image: url("@/assets/images/logo.svg");
+    width: 135px;
+    height: 45px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    opacity: 1;
+    transition: opacity 0.3s cubic-bezier(0.2, 0, 0.1, 1);
+
+    &:hover {
+      opacity: 1;
+    }
+  }
 }
 </style>
