5087a49bf4438f618d16b47e758ba189cab5cfb3
louweal <anneloeslouwe@live.nl>
2023-05-27 22:13:36 +0200

    add lorem ipsum

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,24 @@
 <template>
   <main>
     <hero />
+
+    <section id="about">
+      <div class="container-xl">
+        <div class="row justify-content-center text-center" data-aos="fade-up-scale">
+          <div class="col-12 col-md-8">
+            <h2 class="lh-sm">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque veritatis, hic ullam eum temporibus rem ducimus officiis.
+            </h2>
+          </div>
+          <div class="col-12 col-md-10 fs-md">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem expedita velit consectetur quaerat quam non iste fugit culpa
+              eveniet voluptatum. Hic deleniti itaque temporibus unde quaerat sunt consequatur laborum animi.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
