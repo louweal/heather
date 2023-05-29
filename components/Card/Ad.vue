8b437a6e222adf5dbc44c2192417446439fd2cfc
louweal <anneloeslouwe@live.nl>
2023-05-29 15:55:52 +0200

    added dynamic data

--- components/Card/Ad.vue
+++ components/Card/Ad.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="card overflow-hidden">
-    <visual :data="{ visual: require('~/assets/images/dummy-drill.png'), name: 'Drill' }" />
-    <!-- <img src="..." class="card-img-top" alt="..." /> -->
-    <div class="card-body">
-      <h5 class="card-title">Drill</h5>
-    </div>
+  <div class="card overflow-hidden" v-if="data">
+    <visual :data="{ visual: require(`/assets/images/${data.visuals[0]}`), name: data.title }" />
+    <nuxt-link :to="`/app/detail/${data.id}`" class="card-body">
+      <h5 class="card-title">{{ data.title }}</h5>
+    </nuxt-link>
 
     <div class="card-footer">
       <div class="d-flex gap-2 align-items-center">
-        <div class="bg-light p-2 rounded-circle lh-1"><i class="bi bi-chat-text-fill"></i></div>
+        <button class="bg-light p-2 rounded-circle lh-1 text-white" @click="$store.commit('modals/show', { name: 'request', data: data })">
+          <i class="bi bi-chat-text-fill"></i>
+        </button>
         <div>
-          <div class="lh-1">Anneloes</div>
+          <div class="lh-1">{{ data.name }}</div>
           <div class="lh-sm">Leiden <span class="text-muted fw-medium">1.7km</span></div>
         </div>
       </div>
