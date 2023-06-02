7fc371ba41e48fb8cb458947a96df36f2471f5dc
louweal <anneloeslouwe@live.nl>
2023-06-02 20:49:06 +0200

    add toggle map component for mobile devices

---
+++ components/ToggleMap.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="position-fixed start-50 translate-middle bottom-0 d-md-none" style="z-index: 4">
+    <div class="btn btn-primary" v-if="!$store.state.map.show" @click="$store.commit('map/toggle')">
+      <i class="bi bi-geo-alt-fill"></i> Map
+    </div>
+    <div class="btn btn-primary" v-if="$store.state.map.show" @click="$store.commit('map/toggle')"><i class="bi bi-grid"></i> List</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
