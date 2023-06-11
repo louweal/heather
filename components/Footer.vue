56fd0cdeb8d0f6f63f4efeacc0fae4565b5ada67
louweal <anneloeslouwe@live.nl>
2023-06-11 09:44:52 +0200

    create image attribution page

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="footer bg-light w-100 pt-4 pb-2 mt-3">
     <div class="container-xl font-small">
-      <div class="hstack gap-3">Heather © {{ new Date().getFullYear() }}</div>
+      <div class="hstack gap-3">
+        <span>Heather © {{ new Date().getFullYear() }}</span> <nuxt-link to="attributions">Image attributions</nuxt-link>
+      </div>
     </div>
   </div>
 </template>
---
+++ pages/attributions.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <section>
+      <div class="container-xl">
+        <p>
+          Images by:
+
+          <a href="https://www.vecteezy.com/free-vector/city-skyline">City Skyline Vectors by Vecteezy</a>
+        </p>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
