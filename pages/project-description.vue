1963d99731b3145147226373c5d6ef8dae3f09fe
louweal <anneloeslouwe@live.nl>
2023-06-25 14:19:17 +0200

    wrote project description

---
+++ pages/project-description.vue
@@ -0,0 +1,102 @@
+<template>
+  <main>
+    <div class="container-xl">
+      <div class="row col-12 col-md-10 col-lg-8">
+        <section>
+          <h1>Project description</h1>
+
+          <p>
+            <span class="fw-bold">Team members (1):</span> Anneloes Louwe |
+            <a href="https://www.codesparks.nl" target="_blank">www.codesparks.nl</a> |
+            <a href="https://github.com/louweal" target="_blank"><i class="bi bi-github"></i> louweal</a>
+          </p>
+
+          <p>
+            <span class="fw-bold">Tech:</span> Hedera Javascript SDK <i class="bi bi-dot text-secondary"></i> Hashconnect (Hashpack)
+            <i class="bi bi-dot text-secondary"></i> Solidity <i class="bi bi-dot text-secondary"></i> Vue
+            <i class="bi bi-dot text-secondary"></i> Vuex <i class="bi bi-dot text-secondary"></i> Nuxt
+            <i class="bi bi-dot text-secondary"></i> Bootstrap <i class="bi bi-dot text-secondary"></i> SCSS
+            <i class="bi bi-dot text-secondary"></i> HTML <i class="bi bi-dot text-secondary"></i> Git
+            <i class="bi bi-dot text-secondary"></i> NPM.
+          </p>
+        </section>
+
+        <section>
+          <h2>Summary</h2>
+          <p>
+            Heather is a platform that facilitates borrowing and lending items among neighbors and local communities. It operates as a
+            peer-to-peer sharing platform, allowing individuals to share various items they own with others in their vicinity. The idea
+            behind Heather is to promote sustainability, reduce waste, and foster a sense of community by encouraging people to borrow
+            things they need rather than purchasing them outright.
+          </p>
+          <p>
+            On Heather, users can list items they own but are not currently using, such as tools, electronics, kitchen appliances, camping
+            gear, and much more. When someone needs to borrow an item, they can search for it on Heather's platform and connect with nearby
+            individuals who have the item available for lending. The borrower can send a request, and if the owner agrees, they can arrange
+            the details of the borrowing process, including pickup and return.
+          </p>
+          <p>
+            By utilizing Heather, people can save money, reduce consumption, and minimize their environmental impact by sharing resources
+            within their local community. It promotes a circular economy and encourages a sense of trust and connection among neighbors who
+            participate in the sharing process.
+          </p>
+        </section>
+
+        <section>
+          <h2>Introduction</h2>
+
+          <p>
+            Our society continues to struggle with a pervasive culture of overconsumption, leading to rampant materialism, unnecessary
+            purchases, and the subsequent strain on resources, contributing to rising waste levels, carbon emissions, and a worsening
+            ecological footprint. But at the same time, due to urbanisation, storage space at home is limited, making it increasingly
+            difficult to accommodate infrequently used items or manage bulky belongings. Whilst urban areas become more densely populated,
+            social cohesion within neighborhoods declines rapidly, which makes it increasingly difficult to borrow items from neighbors.
+          </p>
+          <p>
+            There’s still hope. Our small pilot study conducted in The Netherlands revealed that people also care about minimizing their
+            environmental impact, getting to know their neighbors and saving money by borrowing or renting items from their neighbors.
+          </p>
+
+          <p>
+            However, current solutions for sharing resources and improving social cohesion, such as Peerby and Nextdoor struggle with trust
+            and security concerns. For example, when an item lended on Peerby gets damaged, you may get a refund from Peerby. However, this
+            may take a lot of time and also requires Peerby to have a paid subscription model.
+          </p>
+        </section>
+
+        <section>
+          <h2>Problem statement</h2>
+
+          <p>
+            <i
+              >How can we stimulate people to share resources within their local community instead of buying items and not sharing anything
+              with their neighbors?</i
+            >
+          </p>
+        </section>
+
+        <section>
+          <h2>Solving the problem</h2>
+
+          <p>
+            Unlike Peerby, Heather provides a secure sharing platform using smart contract technology on the Hedera network. Users can ask a
+            deposit for each item they share. The deposit is stored in the smart contract whenever someone starts borrowing an item. When
+            the owner of the item reports a problem, the borrower loses his deposit and the owner receives the deposit as a compensation for
+            their damaged or missing item.
+          </p>
+
+          <p>
+            Thanks to Hedera’s secure and fast network with low transaction fees, we were able to create a fully decentralized platform for
+            sharing resources within local communities to minimize environmental impact and to increase social cohesion.
+          </p>
+        </section>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
